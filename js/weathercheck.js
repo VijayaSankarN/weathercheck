6f357dea89c74e0fa9c09d5a2f9ff06a00712e53
VijayaSankarN <sankar.animation@gmail.com>
2015-08-04 11:17:54 +0530

    beautified the code

--- js/weathercheck.js
+++ js/weathercheck.js
@@ -1,33 +1,33 @@
 var app = angular.module('weatherapp', [])
-.controller('MyModuleWeather', function($scope, $http, $log) {
-  $scope.city = 'Trichy';
-  $scope.change = function($la,$lo) {
-    var url = 'http://api.openweathermap.org/data/2.5/weather';
-    $http.jsonp(url, { params : {
-        lat : $la || Drupal.settings.wc_latlong.wc_lat,
-        lon : $lo || Drupal.settings.wc_latlong.wc_long,
-        units : 'metric',
-        callback: 'JSON_CALLBACK'
-      }}).
-      success(function(data, status, headers, config) {
-		$scope.Place = Drupal.settings.wc_latlong.wc_disp[0]=="1"?data.name:'';
-		$scope.Place += Drupal.settings.wc_latlong.wc_disp[1]=="1"?(Drupal.settings.wc_latlong.wc_disp[0]?(','+data.sys.country):data.sys.country):'';
-        $scope.temperature = Drupal.settings.wc_latlong.wc_disp[2]=="1"? "Temperature : " + data.main.temp+"°C<br>":'';
-        $scope.temperature += Drupal.settings.wc_latlong.wc_disp[3]=="1"? "Min " + data.main.temp_min+"°C ":'';
-        $scope.temperature += Drupal.settings.wc_latlong.wc_disp[4]=="1"? "Max " + data.main.temp_max+"°C ":'';
-        $scope.windSpeed = Drupal.settings.wc_latlong.wc_disp[9]=="1"?"Wind Speed : "+data.wind.speed+"m/s":'';
-        $scope.description = Drupal.settings.wc_latlong.wc_disp[5]=="1"?data.weather[0].description:'';
-        $scope.Pressure = Drupal.settings.wc_latlong.wc_disp[6]=="1"?"Pressure : "+data.main.pressure+" hPa":'';
-        $scope.Humidity = Drupal.settings.wc_latlong.wc_disp[7]=="1"?"Humidity : "+data.main.humidity+"%":'';
-        $scope.VisibilityArea = Drupal.settings.wc_latlong.wc_disp[8]=="1"?"Visibility : "+(data.visibility/1000)+" Km":'';
-        $scope.WindDir = Drupal.settings.wc_latlong.wc_disp[10]=="1"?"Wind Direction : "+windDirection(data.wind.deg):'';
-      }).
-      error(function(data, status, headers, config) {
-        $log.error('Could not retrieve data from ' + url);
-      });
-  };
-  $scope.change();
-});
+        .controller('MyModuleWeather', function ($scope, $http, $log) {
+            $scope.city = 'Trichy';
+            $scope.change = function ($la, $lo) {
+                var url = 'http://api.openweathermap.org/data/2.5/weather';
+                $http.jsonp(url, {params: {
+                        lat: $la || Drupal.settings.wc_latlong.wc_lat,
+                        lon: $lo || Drupal.settings.wc_latlong.wc_long,
+                        units: 'metric',
+                        callback: 'JSON_CALLBACK'
+                    }}).
+                        success(function (data, status, headers, config) {
+                            $scope.Place = Drupal.settings.wc_latlong.wc_disp[0] == "1" ? data.name : '';
+                            $scope.Place += Drupal.settings.wc_latlong.wc_disp[1] == "1" ? (Drupal.settings.wc_latlong.wc_disp[0] ? (',' + data.sys.country) : data.sys.country) : '';
+                            $scope.temperature = Drupal.settings.wc_latlong.wc_disp[2] == "1" ? "Temperature : " + data.main.temp + "°C<br>" : '';
+                            $scope.temperature += Drupal.settings.wc_latlong.wc_disp[3] == "1" ? "Min " + data.main.temp_min + "°C " : '';
+                            $scope.temperature += Drupal.settings.wc_latlong.wc_disp[4] == "1" ? "Max " + data.main.temp_max + "°C " : '';
+                            $scope.windSpeed = Drupal.settings.wc_latlong.wc_disp[9] == "1" ? "Wind Speed : " + data.wind.speed + "m/s" : '';
+                            $scope.description = Drupal.settings.wc_latlong.wc_disp[5] == "1" ? data.weather[0].description : '';
+                            $scope.Pressure = Drupal.settings.wc_latlong.wc_disp[6] == "1" ? "Pressure : " + data.main.pressure + " hPa" : '';
+                            $scope.Humidity = Drupal.settings.wc_latlong.wc_disp[7] == "1" ? "Humidity : " + data.main.humidity + "%" : '';
+                            $scope.VisibilityArea = Drupal.settings.wc_latlong.wc_disp[8] == "1" ? "Visibility : " + (data.visibility / 1000) + " Km" : '';
+                            $scope.WindDir = Drupal.settings.wc_latlong.wc_disp[10] == "1" ? "Wind Direction : " + windDirection(data.wind.deg) : '';
+                        }).
+                        error(function (data, status, headers, config) {
+                            $log.error('Could not retrieve data from ' + url);
+                        });
+            };
+            $scope.change();
+        });
 
 function windDirection(num) {
     var val = Math.floor((num / 22.5) + 0.5);
@@ -48,6 +48,6 @@ window.onload = function () {
         draggable: true
     });
     google.maps.event.addListener(marker, 'dragend', function (a) {
-		angular.element(document.getElementById('YourElementId')).scope().change(a.latLng.lat().toFixed(4) , a.latLng.lng().toFixed(4));
+        angular.element(document.getElementById('YourElementId')).scope().change(a.latLng.lat().toFixed(4), a.latLng.lng().toFixed(4));
     });
 };
--- weather-status.tpl.php
+++ weather-status.tpl.php
@@ -1,20 +1,13 @@
-<?php
-/**
- * @file
- * AngularJS template to render a weather block.
- */
-?>
-
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 <div id="map" style="height: 300px; border: 1px solid #000;"></div>
 
 <div ng-app="weatherapp" id="YourElementId" ng-controller="MyModuleWeather">
-  <h3 ng-bind="Place"></h3>
-  <p ng-bind="description"/></span>
-  <p ng-bind-html-unsafe="temperature"></p>
-  <p ng-bind="Pressure"></p>
-  <p ng-bind="Humidity"></p>
-  <p ng-bind="windSpeed"></p>
-  <p ng-bind="WindDir"></p>
-  <p ng-bind="VisibilityArea"></p>
+    <h3 ng-bind="Place"></h3>
+    <p ng-bind="description"/></p>
+    <p ng-bind-html-unsafe="temperature"></p>
+    <p ng-bind="Pressure"></p>
+    <p ng-bind="Humidity"></p>
+    <p ng-bind="windSpeed"></p>
+    <p ng-bind="WindDir"></p>
+    <p ng-bind="VisibilityArea"></p>
 </div>
