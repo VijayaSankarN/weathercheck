2112d0e5cd43ed3c6bd71cb5005b00b2e1427691
VijayaSankarN <sankar.animation@gmail.com>
2015-08-05 14:21:57 +0530

    Updated indents : version 1

--- js/AdminMap.js
+++ js/AdminMap.js
@@ -1,6 +1,9 @@
+/**
+ * Generates map for retrieving the latitude and longitude of a desired location
+ */
 window.onload = function (){
-	var latlng = new google.maps.LatLng(Drupal.settings.wc_latlong.wc_lat, Drupal.settings.wc_latlong.wc_long);
-    var map = new google.maps.Map(document.getElementById('AdminMap'), {
+  var latlng = new google.maps.LatLng(Drupal.settings.wc_latlong.wc_lat, Drupal.settings.wc_latlong.wc_long);
+  var map = new google.maps.Map(document.getElementById('AdminMap'), {
         center: latlng,
         zoom: 11,
         mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -11,7 +14,7 @@ window.onload = function (){
         draggable: true
     });
     google.maps.event.addListener(marker, 'dragend', function (a) {
-        document.getElementById('edit-wc-latitude').value=a.latLng.lat().toFixed(4);
-        document.getElementById('edit-wc-longitude').value=a.latLng.lng().toFixed(4);
+        document.getElementById('edit-wc-latitude').value = a.latLng.lat().toFixed(4);
+        document.getElementById('edit-wc-longitude').value = a.latLng.lng().toFixed(4);
     });
 }
